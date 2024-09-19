37dcf87a625807d9035fb501698756dae419f3f7
itgl <itgel6708@gmail.com>
2024-09-19 15:09:36 +0800

    light

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
